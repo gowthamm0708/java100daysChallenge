83d71dded1f4c8ea9ddb9fe075132cd1700aa6cd
Gowtham <96533662+gowthamm0708@users.noreply.github.com>
2024-05-16 18:50:37 +0530

    Create d2switch.java

---
+++ d2switch.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class d2switch {
+    public static void main(String []args){
+   Scanner sc = new Scanner(System.in); 
+    int day=sc.nextInt();
+switch(day){
+    case 1:
+        System.out.println("MOnday");
+        break;
+    case 2:
+        System.out.println("Tus");
+        break;
+    case 3:
+        System.out.println("Wed");
+        break;
+    case 4:
+        System.out.println("thur");
+        break;
+    case 5:
+        System.out.println("Fri");
+        break;
+    case 6:
+        System.out.println("sat");
+        break;
+    default:
+        System.out.println("Sun");
+        break;
+}
+
+    }
+}
