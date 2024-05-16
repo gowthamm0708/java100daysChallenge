0d2b4de002a32247016a61012452279f4f86aa22
Gowtham <96533662+gowthamm0708@users.noreply.github.com>
2024-05-16 16:12:48 +0530

    Create d2ifelse.java

---
+++ d2ifelse.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class d2ifelse{
+    public static void main(String [] args){
+              
+    int num1 = 40;
+    int num2 = 50;          
+
+if(40 == 50){
+   System.out.println("The num1 and the num2 is equal");
+}
+else{
+       System.out.println("The num1 and the num2 is not equal");
+}
+     
+    }
+
+}
