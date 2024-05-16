ad34391c8433f14bc1c3e41b2d4ee4e5053de680
Gowtham <96533662+gowthamm0708@users.noreply.github.com>
2024-05-16 16:26:42 +0530

    Update d2ifelse.java

--- d2ifelse.java
+++ d2ifelse.java
@@ -1,11 +1,11 @@
 import java.util.*;
 class d2ifelse{
     public static void main(String [] args){
-              
-    int num1 = 40;
-    int num2 = 50;          
+   Scanner sc = new Scanner(System.in);           
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();        
 
-if(40 == 50){
+if(num1 == num2){
    System.out.println("The num1 and the num2 is equal");
 }
 else{
