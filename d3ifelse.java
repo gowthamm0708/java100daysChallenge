12fc93a59f9161f6dc4ef837a9178ce47d8c3556
Gowtham <96533662+gowthamm0708@users.noreply.github.com>
2024-05-17 10:15:25 +0530

    Create d3ifelse.java

---
+++ d3ifelse.java
@@ -0,0 +1,22 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+   String RCB = sc.nextLine();
+   if(RCB.equals("Win")){
+   
+   System.out.println("esala cub namathe");
+   }
+   else if(RCB.equals("loose")){
+       System.out.println("athu avolotha");
+   }
+   else{
+   
+    System.out.println("cub ella" );
+   
+    
+    }
+    }
+}
