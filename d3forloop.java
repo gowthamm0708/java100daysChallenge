fc2f5530928ac6b46bd762e0be18e2552a9a87e8
Gowtham <96533662+gowthamm0708@users.noreply.github.com>
2024-05-17 19:23:59 +0530

    Create d3forloop.java

---
+++ d3forloop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+  Scanner sc =new Scanner(System.in);
+  int a = sc.nextInt();
+  int b = sc.nextInt();
+  
+   for(a=a ;a<=b;a++){
+       System.out.println(a);
+   }
+   
+    }
+}
