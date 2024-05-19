18d4d8618aeb9e449fb4b4a5590ee0f9a4064523
Gowtham <96533662+gowthamm0708@users.noreply.github.com>
2024-05-19 14:44:53 +0530

    Create d4.ternaryoperrater.java

---
+++ d4.ternaryoperrater.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+  Scanner sc =new Scanner(System.in);
+  int a = sc.nextInt();
+  int b = sc.nextInt();
+  String num1 = a >= b? "a is greater than b" : "B is greater then a";
+  System.out.println(num1);
+   
+   
+    }
+}
