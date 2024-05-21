f23203502aa431de88f4206395be608dff0fbf43
Gowtham <96533662+gowthamm0708@users.noreply.github.com>
2024-05-21 23:01:45 +0530

    Create d6funtionswithparameters.java

---
+++ d6funtionswithparameters.java
@@ -0,0 +1,23 @@
+
+public class addtion {
+   void sum(int a,int b){
+System.out.println(a+b);
+   }
+   void sub(int a,int b){
+System.out.println(a-b);
+   }
+   void mul(int a,int b){
+System.out.println(a*b);
+   }
+   void div(int a,int b){
+System.out.println(a/b);
+   }
+    public static void main(String[] args) {
+        addtion operation = new addtion();
+            operation.sum(20,20);
+            operation.sub(20,20);
+            operation.mul(20,20);
+            operation.div(20,20);
+
+    }
+}
