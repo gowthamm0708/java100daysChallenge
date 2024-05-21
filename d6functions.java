04b5ec3a62b361c3c1550e97474dae434488ef79
Gowtham <96533662+gowthamm0708@users.noreply.github.com>
2024-05-21 22:07:41 +0530

    Create d6functions.java

---
+++ d6functions.java
@@ -0,0 +1,22 @@
+
+public class mobile {
+   void iphone(){
+        System.out.println("iphone");
+   }
+   void samsung(){
+        System.out.println("samsung");
+   }
+   void realme(){
+        System.out.println("realme");
+   }
+   void asus(){
+        System.out.println("asus");
+   }
+    public static void main(String[] args) {
+        mobile brand = new mobile();
+       brand.iphone();
+       brand.samsung();
+        brand.realme();
+        brand.asus();
+    }
+}
