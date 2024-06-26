466c6c9741cd2e588fba336a0fa69eda28f3be01
Gowtham <gowthamm0708@gmail.com>
2024-06-26 22:58:54 +0530

    Create d13pattern.java

---
+++ d13pattern.java
@@ -0,0 +1,16 @@
+package gowtham;
+
+import java.util.*;
+public class patternprinting {
+	public static void main(String[] args) {
+	  
+		for(int i = 1; i<=5;i++) {
+			for(int j =i;j<=5;j++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+
+	}
+
+}
