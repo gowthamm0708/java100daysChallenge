4dfbb221025efb6ce715bd9dfe0de09b24e6ed08
Gowtham <gowthamm0708@gmail.com>
2024-07-31 23:08:35 +0530

    Create d15userinputArray.java

---
+++ d15userinputArray.java
@@ -0,0 +1,15 @@
+package gowtham;
+import java.util.*;
+public class breakUsingWhile {
+public static void main(String[]args) {
+	 int a[] = new int[5]; 
+	 Scanner sc = new Scanner(System.in);
+	 for(int i =0; i<a.length; i++) {
+         a[i] = sc.nextInt();
+	 }
+	 for(int i = 0; i<a.length;i++) {
+         System.out.println(a[i]);
+	 }
+
+}
+}
