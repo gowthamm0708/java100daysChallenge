ef2bdfb10fabe643a083be0255a4e6b323438c8c
Gowtham <gowthamm0708@gmail.com>
2024-08-01 23:53:52 +0530

    Create d15SortingIntegerAndString.java

---
+++ d15SortingIntegerAndString.java
@@ -0,0 +1,13 @@
+package gowtham;
+import java.util.*;
+public class breakUsingWhile {
+public static void main(String[]args) {
+	int array1[] = {59,34,76,23,54,887,232,54,75,5,24,35,5};
+	String array2[]= {"java","python","js","c++","csharp"};
+	Arrays.sort(array1);
+	Arrays.sort(array2);
+	System.out.println("sorted integer"+" "+Arrays.toString(array1));
+	System.out.println("sorted String"+" "+Arrays.toString(array2));
+
+}
+}
