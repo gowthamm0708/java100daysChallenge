0a6635d4aba37f4c3e2194fef40eee5e31ff439c
Gowtham <gowthamm0708@gmail.com>
2024-08-01 21:11:49 +0530

    Create d15ReverseString.java

---
+++ d15ReverseString.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class HelloWorld {
+   public static void main(String[] args) {
+        String name = "gowtham";
+     StringBuilder sb =new StringBuilder();
+        sb.append(name);
+         sb.reverse();
+        System.out.println(sb);
+        }
+} 
