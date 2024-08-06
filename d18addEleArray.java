b9f7bd99421ae1bf5db6b8686013f97af6449a18
Gowtham <gowthamm0708@gmail.com>
2024-08-06 22:37:11 +0530

    Create d18addEleArray.java

---
+++ d18addEleArray.java
@@ -0,0 +1,13 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        int arr [] = {5,6,7,9,10};
+        int sum=0;
+        for(int i =0; i<arr.length; i++){
+            sum +=arr[i];
+        }
+        System.out.println(sum);
+    }
+}
