cb12c46c7c47b188c11159a6a6ed132fb005bd45
Gowtham <gowthamm0708@gmail.com>
2024-08-09 22:31:21 +0530

    Create d19using2DArray.java

---
+++ d19using2DArray.java
@@ -0,0 +1,20 @@
+package gowtham;
+import java.util.*;
+public class breakUsingWhile {
+public static void main(String[]args) {
+	int array1[][] = new int[10][10];
+//	int sum = 0;
+	for(int i =0;i<10;i++) {
+		for(int j = 0; j<10;j++) {
+			System.out.print( array1[i][j]);	
+		} 
+		System.out.println();
+		//		if(array1[i]>min)
+//			sum = sum + array1[i];
+	}
+	
+//	System.out.println(sum);
+	
+
+}
+}
