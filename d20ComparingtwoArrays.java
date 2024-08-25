65ff1ca08b97ba687e99a83b539bc3452a5124b8
Gowtham <gowthamm0708@gmail.com>
2024-08-25 08:30:55 +0530

    Create d20ComparingtwoArrays.java

---
+++ d20ComparingtwoArrays.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        int [] arr1 = {1,2,3,4,5};
+        int [] arr2 = {1,2,5,4,5};
+        
+        if(Arrays.equals(arr1,arr2)){
+            
+            System.out.println("Truee");
+        }else{
+            System.out.println("not true");
+        }
+    
+        
+    }
+}
