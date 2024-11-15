21206c92d328d082fb6f8229c3450f8089e6c163
Gowtham <gowthamm0708@gmail.com>
2024-11-15 22:09:16 +0530

    Create p4Factorial.java

---
+++ java basic programs/p4Factorial.java
@@ -0,0 +1,19 @@
+package gowtham;
+import java.util.*;
+public class addtheNumbers {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+	       int num = sc.nextInt();
+	       int factorial = 1;
+	      
+	        for(int i=1;i<=num;i++){
+	             factorial *=i;
+	           
+	            
+	        }
+	    System.out.println(factorial);
+	} 
+
+}
+ 
